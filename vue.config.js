43287cf71b20646792df9a8ef2f11819e9162a7b
Hebh <yaheaawam3@gmail.com>
2023-07-18 07:12:32 +0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports =
-    publicPath: process.env.NODE_ENV === "production" ? "/vue_course/" : "/",
-    defineConfig({transpileDependencies: true,});
-
+module.exports = defineConfig({
+    transpileDependencies: true,
+});
